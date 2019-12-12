33e59fc2a1ae573aec965f24a277017e39b8a1b7
João Martins <up201707311@fe.up.pt>
2019-12-12 12:30:02 +0000

    optimized

--- Exercicios/10/plop2.pl
+++ Exercicios/10/plop2.pl
@@ -5,16 +5,13 @@ lazy_man(Seq,N,Total) :-
   domain(Seq,1,N),
   all_distinct(Seq),
   element(N, Seq, 6),
-  NTrips is N-1,
-  length(Ts,NTrips),
-  trip_time(Ts,Seq),
-  sum(Ts, #=, Total),
+  trip_time(Seq,Total),
   labeling([maximize(Total)],Seq).
 
-trip_time([],[_]).
-trip_time([T|Ts],[S1,S2|Ss]) :-
-  T #= abs(S2-S1),
-  trip_time(Ts,[S2|Ss]).
+trip_time([_],0).
+trip_time([S1,S2|Ss],T) :-
+  T #= T2 + abs(S2-S1),
+  trip_time([S2|Ss],T2).
 
   
   
